c7de9b00212fedc1fe4d985d192827635adbd589
davifelipef <feliperedfield@gmail.com>
2023-12-06 12:55:18 -0300

    Update main.py
    Code refactoring

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 import os
 from PIL import Image
 
+# Configuration variables
+
+# Defines the name of the input and output folders
+input_folder = 'input'
+output_folder = 'output'
+# Defines the percentage the image should be scaled to
+upscale_percentage = 400
+# Defines the supported input file extensions
+supported_extensions = ('.jpg', '.png', '.jpeg', '.bmp')
+# Defines the desired output format. Default: BMP
+output_format = 'BMP'
+
+# Function that upscales the image
 def upscale_image(input_image_path, output_image_path, scale_percent):
-    # Opens the image
     image = Image.open(input_image_path)
+
+    # Shows a message if the file is not found
+    if not os.path.exists(input_image_path):
+        raise FileNotFoundError(f"File '{input_image_path}' not found.")
     
-    # Defines the new size of the image
+    # Lowers the text of the file extension for easy universal handling
+    file_extension = os.path.splitext(input_image_path)[1].lower()
+    
+    # Raises an error if the file extension is not listed in the suported extensions
+    if file_extension not in supported_extensions:
+        raise ValueError("File format not supported.")
+    
+    # Sets the image size
     width, height = image.size
     new_width = int(width * (scale_percent / 100))
     new_height = int(height * (scale_percent / 100))
     new_size = (new_width, new_height)
     
-    # Resizes the image using a high-quality resampling method
-    resized_image = image.resize(new_size, Image.NEAREST)  
-    
-    # Saves the resized image as a .bmp file
-    resized_image.save(output_image_path, 'BMP')
+    # Resizes the image using a high-quality resampling method 
+    resized_image = image.resize(new_size, Image.NEAREST)
 
-# Path to the input and output folders. Here they are in the same folder as the py file
-input_folder = 'input'
-output_folder = 'output'
+    # Saves the resized image in the desired output format
+    resized_image.save(output_image_path, output_format)
 
 # Processes each file in the input folder
 for filename in os.listdir(input_folder):
-    if filename.endswith(('.jpg', '.png', '.jpeg')):  
-        input_path = os.path.join(input_folder, filename)
-        
-        # Generates the output filename with .bmp extension
-        output_filename = os.path.splitext(filename)[0] + '.bmp'
-        output_path = os.path.join(output_folder, output_filename)
-        
-        # Upscales the image to 400% and save as a .bmp file, as previously set
-        upscale_image(input_path, output_path, 400)
+    input_path = os.path.join(input_folder, filename)
+    # Generates the output filename with the desired file extension
+    output_filename = os.path.splitext(filename)[0] + f'.{output_format.lower()}'
+    output_path = os.path.join(output_folder, output_filename)
+    
+    # Upscales the image based on the specified percentage and saves it
+    upscale_image(input_path, output_path, upscale_percentage)
